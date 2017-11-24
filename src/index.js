a90c64e7e404cc876e26d041b637fce8e3d2cd37
Christian Gaetano <hello@christiangaetano.com>
2017-11-24 11:35:24 -0800

    Add JSDoc for the base API export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import Quantity from './lib/Quantity';
 
-module.exports = input => new Quantity(input);
+/**
+ * Create a new Quantity object to access conversion functions.
+ * @param {string} inputMeasurement The base measurement to convert from in the form <quantity> <unit>.
+ * For example, 12 cups. Quantities can be integers or decimals, so 15.5 tablespoons is also
+ * acceptable. The interpreter also understands most abbreviated units, e.g. 1 oz.
+ */
+module.exports = inputMeasurement => new Quantity(inputMeasurement);
